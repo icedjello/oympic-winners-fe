b1a50ba35b9951ceedb12f42ad92b55abbd5235c
bamfard@gmail.com <^,+jTk'(wiq"ybh&Ga5[y>
2021-10-07 16:33:00 +0100

    Final Commit, version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,13 @@ import {
   GridApi,
   GridReadyEvent,
   IServerSideGetRowsParams,
-  Module, RowNode, ServerSideTransactionResult, ValueParserParams
+  Module,
+  RowNode,
+  ServerSideTransactionResult,
+  SetFilterValuesFuncParams,
+  ValueParserParams,
+  ColumnApi
 } from "@ag-grid-community/core";
-import {ColumnApi} from "@ag-grid-community/core";
 import {ServerSideRowModelModule} from "@ag-grid-enterprise/server-side-row-model";
 import {MenuModule} from "@ag-grid-enterprise/menu";
 import {ColumnsToolPanelModule} from "@ag-grid-enterprise/column-tool-panel";
@@ -20,8 +24,8 @@ import {MatDialog} from "@angular/material/dialog";
 @Component({
   selector: 'app-root',
   styles: [`#myGrid {
-    width: 1000px;
-    height: 1000px;
+    width: 1600px;
+    height: 1200px;
   }`],
   template: `
     <div>
@@ -50,7 +54,6 @@ import {MatDialog} from "@angular/material/dialog";
   `,
 })
 
-
 export class AppComponent {
   public readonly rowModelType = 'serverSide';
   public readonly serverSideStoreType = 'partial';
@@ -61,7 +64,6 @@ export class AppComponent {
   public readonly autoGroupColumnDef: ColDef = {flex: 2, filter: false, sortable: false}
   public readonly columnTypes: {};
   public readonly rowSelection = 'single'
-  public rowData: {}[];
   public getServerSideStoreParams: (params: GetServerSideStoreParamsParams) => void = params => {
     const groupingActive = !(params.rowGroupColumns.length === 0);
     if (groupingActive) {
@@ -78,8 +80,11 @@ export class AppComponent {
       };
     }
   }
+  public rowData: {}[];
+
   private gridApi?: GridApi;
   private columnApi?: ColumnApi;
+
 
   constructor(private http: HttpClient, private dialog: MatDialog) {
     this.rowData = [];
@@ -99,16 +104,12 @@ export class AppComponent {
       setFilterColumn: {
         filter: 'agSetColumnFilter',
         filterParams: {
-          values: (params: any) => { //fix any
-            console.log('from filterParams: FIX ME!', params)
-          }
+          buttons: ['apply', 'reset'],
+          refreshValuesOnOpen: true,
+          values: this.getSetFilterValues.bind(this)
         }
-      }
-    };
-    this.columnDefs = [
-      // {field: 'id'}, // this column exists for debugging purposes.
-      {
-        field: 'athlete',
+      },
+      textFilterColumn: {
         filter: 'agTextColumnFilter',
         filterParams: {
           buttons: ['apply', 'reset'],
@@ -116,6 +117,13 @@ export class AppComponent {
           suppressAndOrCondition: true,
           filterOptions: ['contains']
         },
+      }
+    };
+
+    this.columnDefs = [
+      {
+        field: 'athlete',
+        type: 'textFilterColumn',
         flex: 2
       },
       {
@@ -124,8 +132,8 @@ export class AppComponent {
       },
       {
         field: 'country',
-        type: 'setFilterColumn',
         flex: 1.5,
+        type: 'setFilterColumn',
       },
       {
         field: 'sport',
@@ -147,9 +155,16 @@ export class AppComponent {
     ];
   }
 
+  private getSetFilterValues(params: SetFilterValuesFuncParams): void {
+    const body = {field: params.colDef.field, filterModel: this.gridApi?.getFilterModel()}
+    this.http.post('http://localhost:3000/setFilterValues', body).subscribe(data => params.success(data as string[]))
+  }
+
   private isGroupingActive: () => boolean = () => !(this.columnApi?.getRowGroupColumns().length === 0);
 
-  private createServerSideData(newRowData: {}) {
+  private noGroupingRefresh: () => void = () => this.gridApi?.refreshServerSideStore({purge: false});
+
+  private createServerSideData(newRowData: {}): void {
     this.http.post('http://localhost:3000/create', newRowData)
       .subscribe((resMsg) => {
           console.log(resMsg)
@@ -158,26 +173,12 @@ export class AppComponent {
     if (this.isGroupingActive()) {
       const result: ServerSideTransactionResult = this.gridApi?.applyServerSideTransaction({
           add: [newRowData],
-          // route: this.getRoute(node)
         },
       ) as ServerSideTransactionResult
       console.log(result)
     } else {
-      this.gridApi?.refreshServerSideStore(
-        {purge: false}
-      )
+      this.noGroupingRefresh();
     }
-  }
-
-  public openDialog() {
-    let dialogRef = this.dialog.open(DialogElementsExampleDialog);
-    dialogRef.afterClosed().subscribe(newRowData => {
-      this.createServerSideData(newRowData)
-    })
-  }
-
-  public numberValueParser(params: ValueParserParams): number {
-    return Number(params.newValue);
   }
 
   private updateServerSideData(id: number, updateData: {}): void {
@@ -200,32 +201,6 @@ export class AppComponent {
     return route;
   }
 
-  public onDelete(): void {
-    if (this.gridApi) {
-      const gridApi = this.gridApi;
-      const node: RowNode = gridApi.getSelectedNodes()[0]
-      const nodeId = node.data.id;
-
-      if (this.isGroupingActive()) {
-        // we have grouping, thus need a route...
-        this.deleteServerSideData(nodeId);
-        const result: ServerSideTransactionResult = gridApi.applyServerSideTransaction({
-            remove: [node.data],
-            route: this.getRoute(node)
-          },
-        ) as ServerSideTransactionResult
-        console.log(result)
-        // gridApi.refreshServerSideStore({route: route, purge: true});
-      } else {
-        this.deleteServerSideData(nodeId);
-        gridApi.refreshServerSideStore({
-          // route: this.getRoute(node),
-          purge: false
-        })
-      }
-    }
-  }
-
   private deleteServerSideData(id: number): void {
     const body = {id};
     this.http.post('http://localhost:3000/delete', body)
@@ -234,7 +209,39 @@ export class AppComponent {
       })
   }
 
-  public onCellEditingStopped(params: CellEditingStoppedEvent) {
+
+  public openDialog(): void {
+    let dialogRef = this.dialog.open(DialogElementsExampleDialog);
+    dialogRef.afterClosed().subscribe(newRowData => {
+      this.createServerSideData(newRowData)
+    })
+  }
+
+  public numberValueParser(params: ValueParserParams): number {
+    return Number(params.newValue);
+  }
+
+  public onDelete(): void {
+    if (this.gridApi) {
+      const gridApi = this.gridApi;
+      const node: RowNode = gridApi.getSelectedNodes()[0]
+      const nodeId = node.data.id;
+      this.deleteServerSideData(nodeId);
+
+      if (this.isGroupingActive()) {
+        const result: ServerSideTransactionResult = gridApi.applyServerSideTransaction({
+            remove: [node.data],
+            route: this.getRoute(node)
+          },
+        ) as ServerSideTransactionResult
+        console.log(result)
+      } else {
+        this.noGroupingRefresh();
+      }
+    }
+  }
+
+  public onCellEditingStopped(params: CellEditingStoppedEvent): void {
     this.updateServerSideData(params.data.id, params.data);
     params.node.setData(params.data);
   }
@@ -251,11 +258,9 @@ export class AppComponent {
         that.http.post('http://localhost:3000/read', body)
           .subscribe((dataAndCount) => {
             const receivedData = dataAndCount as { rows: {}[], count: number }
-            const rows = receivedData.rows;
-            const count = receivedData.count
             params.success({
-              rowData: rows,
-              rowCount: count
+              rowData: receivedData.rows,
+              rowCount: receivedData.count
             });
           })
         return;
@@ -263,4 +268,3 @@ export class AppComponent {
     })
   }
 }
-
